1508d2ac96a2ea55896ed7d4a43201b0761533cd
Jonas Raoni Soares da Silva <github@raoni.org>
2023-07-08 16:34:17 +0300

    #57 Formatting

--- PLNPluginSchemaMigration.php
+++ PLNPluginSchemaMigration.php
@@ -8,6 +8,7 @@
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class PLNPluginSchemaMigration
+ *
  * @brief Describe database table structures.
  */
 
@@ -18,68 +19,70 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class PLNPluginSchemaMigration extends Migration {
-	/**
-	 * Run the migrations.
-	 * @return void
-	 */
-	public function up() {
-		// PLN Deposit Objects
-		if (!Schema::hasTable('pln_deposit_objects')) {
-			Schema::create('pln_deposit_objects', function (Blueprint $table) {
-				$table->bigInteger('deposit_object_id')->autoIncrement();
-				$table->bigInteger('journal_id');
-				$table->bigInteger('object_id');
-				$table->string('object_type', 36);
-				$table->bigInteger('deposit_id')->nullable();
-				$table->datetime('date_created');
-				$table->datetime('date_modified')->nullable();
-			});
-		}
+class PLNPluginSchemaMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        // PLN Deposit Objects
+        if (!Schema::hasTable('pln_deposit_objects')) {
+            Schema::create('pln_deposit_objects', function (Blueprint $table) {
+                $table->bigInteger('deposit_object_id')->autoIncrement();
+                $table->bigInteger('journal_id');
+                $table->bigInteger('object_id');
+                $table->string('object_type', 36);
+                $table->bigInteger('deposit_id')->nullable();
+                $table->datetime('date_created');
+                $table->datetime('date_modified')->nullable();
+            });
+        }
 
-		// PLN Deposits
-		if (!Schema::hasTable('pln_deposits')) {
-			Schema::create('pln_deposits', function (Blueprint $table) {
-				$table->bigInteger('deposit_id')->autoIncrement();
-				$table->bigInteger('journal_id');
-				$table->string('uuid', 36)->nullable();
-				$table->bigInteger('status')->default(0)->nullable();
-				$table->string('staging_state')->nullable();
-				$table->string('lockss_state')->nullable();
-				$table->datetime('date_status')->nullable();
-				$table->datetime('date_created');
-				$table->datetime('date_modified')->nullable();
-				$table->string('export_deposit_error', 1000)->nullable();
-				$table->datetime('date_preserved')->nullable();
-			});
-		}
+        // PLN Deposits
+        if (!Schema::hasTable('pln_deposits')) {
+            Schema::create('pln_deposits', function (Blueprint $table) {
+                $table->bigInteger('deposit_id')->autoIncrement();
+                $table->bigInteger('journal_id');
+                $table->string('uuid', 36)->nullable();
+                $table->bigInteger('status')->default(0)->nullable();
+                $table->string('staging_state')->nullable();
+                $table->string('lockss_state')->nullable();
+                $table->datetime('date_status')->nullable();
+                $table->datetime('date_created');
+                $table->datetime('date_modified')->nullable();
+                $table->string('export_deposit_error', 1000)->nullable();
+                $table->datetime('date_preserved')->nullable();
+            });
+        }
 
-		// Create a new scheduled_tasks entry for this plugin
-		DB::table('scheduled_tasks')->insertOrIgnore(['class_name' => 'plugins.generic.pln.classes.tasks.Depositor']);
+        // Create a new scheduled_tasks entry for this plugin
+        DB::table('scheduled_tasks')->insertOrIgnore(['class_name' => 'plugins.generic.pln.classes.tasks.Depositor']);
 
-		$this->upgrade();
-	}
+        $this->upgrade();
+    }
 
-	/**
-	 * Upgrade and fixes
-	 */
-	protected function upgrade() {
-		// Before the version 2.0.4.3, it's needed to check for a missing "export_deposit_error" field
-		if (!Schema::hasColumn('pln_deposits', 'export_deposit_error')) {
-			Schema::table('pln_deposits', function (Blueprint $table) {
-				$table->string('export_deposit_error', 1000)->nullable();
-			});
-		}
+    /**
+     * Upgrade and fixes
+     */
+    protected function upgrade()
+    {
+        // Before the version 2.0.4.3, it's needed to check for a missing "export_deposit_error" field
+        if (!Schema::hasColumn('pln_deposits', 'export_deposit_error')) {
+            Schema::table('pln_deposits', function (Blueprint $table) {
+                $table->string('export_deposit_error', 1000)->nullable();
+            });
+        }
 
-		// Changes introduced at version 3.0.0.0
-		if (!Schema::hasColumn('pln_deposits', 'date_preserved')) {
-			Schema::table('pln_deposits', function (Blueprint $table) {
-				$table->datetime('date_preserved')->nullable();
-				$table->string('staging_state')->nullable();
-				$table->string('lockss_state')->nullable();
-			});
-			// Reset status
-			DB::table('pln_deposits')->update(['status', null]);
-		}
-	}
+        // Changes introduced at version 3.0.0.0
+        if (!Schema::hasColumn('pln_deposits', 'date_preserved')) {
+            Schema::table('pln_deposits', function (Blueprint $table) {
+                $table->datetime('date_preserved')->nullable();
+                $table->string('staging_state')->nullable();
+                $table->string('lockss_state')->nullable();
+            });
+            // Reset status
+            DB::table('pln_deposits')->update(['status', null]);
+        }
+    }
 }
